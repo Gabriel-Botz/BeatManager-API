08bee040bcbebf791cb92232a923d52a2b50e5d3
Gabriel Botelho <gabriel.c.botelho@hotmail.com>
2026-08-11 12:38:48 -0300

    chore: subindo estrutura

---
+++ src/main/java/br/com/gabriel/beatmanager/BeatmanagerApplication.java
@@ -0,0 +1,13 @@
+package br.com.gabriel.beatmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeatmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeatmanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gabriel/beatmanager/BeatmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gabriel.beatmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeatmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
